de69c890d67d2d39bdd9550ef071d67c65d14d15
andresapao <andreoliveira1982@gmail.com>
2026-05-15 19:11:13 -0300

    feat: get endpoint fetching currency

--- src/main/kotlin/org/transactions/currency/service/TransactionService.kt
+++ src/main/kotlin/org/transactions/currency/service/TransactionService.kt
@@ -7,8 +7,9 @@ import org.transactions.currency.gateway.CurrencyGatewayImpl
 import org.transactions.currency.model.TransactionCurrencyResponse
 import org.transactions.currency.model.TransactionRequest
 import org.transactions.currency.repository.TransactionRepository
-import java.math.BigDecimal
+import java.math.RoundingMode
 import java.time.format.DateTimeFormatter
+import kotlin.time.measureTimedValue
 
 
 @Service
@@ -16,6 +17,7 @@ class TransactionService(
     private val repository: TransactionRepository,
     private val currencyGatewayImpl: CurrencyGatewayImpl
 ) {
+    //private val logger = KotlinLogging.logger {}
     fun create(request: TransactionRequest) = repository.save(request.toEntity())
     fun getTransactionWithExchangeRate(id: Long, targetCurrency: String): TransactionCurrencyResponse {
         val transaction = repository.findById(id).orElseThrow({
@@ -27,20 +29,18 @@ class TransactionService(
 
         val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
         val formattedDate = transaction.creationDate.minusMonths(6).format(formatter)
-
         val filter: String = "record_date:gte:${formattedDate},currency:eq:${targetCurrency}&sort:-record_date"
-        val exchangeRate = currencyGatewayImpl.getValueAtCurrency(filter).get(0)
-        println(exchangeRate)
-        return TransactionCurrencyResponse(
-            id = 5,
-            originalValue = BigDecimal.TEN,
-            description = "yyy",
-            exchangeRate = BigDecimal.TEN,
-            convertedValue = BigDecimal.TEN,
-            creationDate = transaction.creationDate
+        val (exchangeRate, elapsedTime) = measureTimedValue { currencyGatewayImpl.getValueAtCurrency(filter).get(0) }
 
+        println("Elapsed time for getting rate: " + elapsedTime.inWholeMilliseconds + " ms")
+        return TransactionCurrencyResponse(
+            id = transaction.id!!,
+            originalValue = transaction.amount,
+            description = transaction.description,
+            exchangeRate = exchangeRate.rate,
+            convertedValue = (transaction.amount * exchangeRate.rate).setScale(2, RoundingMode.HALF_UP),
+            creationDate = transaction.creationDate
         )
     }
-
 }
 
